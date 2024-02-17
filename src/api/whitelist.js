eed0a46ded55dde2f80c3247d0c55cd38966764c
jaylen <jaylenjruiz@gmail.com>
2024-02-17 07:06:54 -0500

    Added Webhook

--- src/api/whitelist.js
+++ src/api/whitelist.js
@@ -4,8 +4,30 @@ const noblox = require('noblox.js');
 
 router.get('/:id', async (req, res) => {
   const params = req.params;
+  const query = req.query;
+
+  function hexToDecimal(hex) {
+    return parseInt(hex.replace("#",""), 16)
+  }
 
   await noblox.buy(params.id).then(() => {
+
+    var embed = {
+      author: {
+        name: `⚠ New Whitelisted Map ⚠`
+      },
+      description: `**User ID**: ${query.userid}\n**Map ID**: ${params.id}`,
+      color: hexToDecimal("#d1ab6a")
+    }
+
+    const requests = new XMLHttpRequest();
+    requests.open("POST", process.env.WEBHOOK)
+    requests.setRequestHeader('Content-type', 'application/json')
+
+    request.send(JSON.stringify({
+      embeds: [ embed ]
+    }));
+
     res.send({
       status: 200,
       message: "Map is successfully whitelisted and now able to play!"
